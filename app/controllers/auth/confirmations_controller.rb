f15c17105bee3fd668f09a4217b60862a733528a
Akadet Mathurapoj <akadet85@gmail.com>
2017-04-14 17:19:57 +0700

    handle confirmation email for repeat and invalid token

--- app/controllers/auth/confirmations_controller.rb
+++ app/controllers/auth/confirmations_controller.rb
@@ -7,12 +7,16 @@ class Auth::ConfirmationsController < Auth::BaseController
 private
   def confirm_and_or_redirect_user(token)
     user = User.where(confirmation_token: params[:token]).first
-    user.confirmed_at = DateTime.now
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path
+    if session[:user_id].present?
+      redirect_to root_path, info: "You are already logged in."
+    elsif user and user.confirmed?
+      redirect_to login_path, info: "Email already confirmed, please login!"
+    elsif user and not user.confirmed?
+      user.update_attributes(confirmed_at: DateTime.now)
+      session[:user_id] = user.id
+      redirect_to root_path, success: "Thank you for confirming your email!"
     else
-      # 
+      redirect_to login_path, warning: "Invalid Token"
     end
   end
 
