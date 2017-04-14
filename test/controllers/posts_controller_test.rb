25b05594195c6cc94e8a96ae4b254ca19f88bab6
Akadet Mathurapoj <akadet85@gmail.com>
2017-04-14 18:08:45 +0700

    add post unit test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
-class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class PostsControllerTest < ActionController::TestCase
+  test "index action should be success" do
+    get :index
+    assert_response :success
+  end
+
+  test "new should be success" do
+    get :new
+    assert_response :success
+  end
+
+  test "show should be success" do
+    post = posts(:hello_world)
+
+    get :show, params: { id: post.id }
+    assert_response :success
+  end
 end
