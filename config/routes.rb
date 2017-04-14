b3d3e5f0d7f265bf2ee8cae2a692f4289b747137
Akadet Mathurapoj <akadet85@gmail.com>
2017-04-14 16:42:00 +0700

    extract user, session and confirmation to auth namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create]
+  
+  namespace :auth do
+    resources :users, only: [:create]
+    resource :session, only: [:create]
+    resource :confirmation
+  end
 
-  resource :session, only: [:create]
-
-  resource :confirmation
-
-  get 'login' => 'sessions#new'
-  delete 'logout' => 'sessions#destroy'
-  get 'register' => 'users#new'
+  get 'login' => 'auth/sessions#new'
+  delete 'logout' => 'auth/sessions#destroy'
+  get 'register' => 'auth/users#new'
 
   resources :posts do
     resources :comments
