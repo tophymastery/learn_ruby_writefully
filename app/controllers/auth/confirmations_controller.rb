b82ca05ffef99557fb86af6096b17f233a5ae021
Akadet Mathurapoj <akadet85@gmail.com>
2017-04-14 17:08:03 +0700

    refactor confirmation and allow only new method

--- app/controllers/auth/confirmations_controller.rb
+++ app/controllers/auth/confirmations_controller.rb
@@ -1,21 +1,13 @@
 class Auth::ConfirmationsController < Auth::BaseController
 
   def new
-    if params[:token].present?
-      confirm_and_redirect_user(params[:token])
-    else
-      # 
-    end
-  end
-
-  def create
-    confirm_and_redirect_user(params[:token])
+    confirm_and_or_redirect_user(params[:token]) if params[:token].present?
   end
 
 private
-  def confirm_and_redirect_user(token)
-    @user = User.where(confirmation_token: params[:token]).first
-    @user.confirmed_at = DateTime.now
+  def confirm_and_or_redirect_user(token)
+    user = User.where(confirmation_token: params[:token]).first
+    user.confirmed_at = DateTime.now
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
