834dd256b3b50b77bcb83b15195b0ac3927e0d33
Akadet Mathurapoj <akadet85@gmail.com>
2017-04-15 14:37:33 +0700

    add unit test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "email should not be valid" do
+    user = User.new(email: 'invalid', 
+                    password: 'tophy', 
+                    password_confirmation: 'tophy')
+
+    assert_not user.valid?
+  end
+
+  test "email should be valid" do
+    user = User.new(email: 'valid@gmail.com', 
+                    password: 'tophy', 
+                    password_confirmation: 'tophy')
+
+    assert user.valid?
+  end
+
+  test "user should have confirmation token" do
+    user = User.create(email: 'valid@gmail.com', 
+                    password: 'tophy', 
+                    password_confirmation: 'tophy')
+
+    assert_not_nil user.confirmation_token
+  end
 end
