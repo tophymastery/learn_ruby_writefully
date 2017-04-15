fae4a7ca7da37b04ca9a5b0e32ad336421cb07d1
Akadet Mathurapoj <akadet85@gmail.com>
2017-04-16 00:00:15 +0700

    add filter post status

---
+++ app/helpers/my_admin/posts_helper.rb
@@ -0,0 +1,15 @@
+module MyAdmin
+  module PostsHelper
+    class << self
+      def render_states(view)
+        view.sanitize(
+          Post.statuses.keys.map do |status|
+            view.content_tag :li do
+              view.link_to status, '#'
+            end
+          end.join
+        )
+      end
+    end
+  end
+end
