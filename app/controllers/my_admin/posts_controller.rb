5bd3e67f5b028a0a30d57c543a3db58842e7605d
Akadet Mathurapoj <akadet85@gmail.com>
2017-04-15 16:35:47 +0700

    move post create, edit, delete to my_admin module

--- app/controllers/my_admin/posts_controller.rb
+++ app/controllers/my_admin/posts_controller.rb
@@ -3,4 +3,45 @@ class MyAdmin::PostsController < MyAdmin::BaseController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.where(id: params[:id]).first
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      # render form again
+    end
+  end
+
+  def update
+    @post = Post.where(id: params[:id]).first
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      # render edit form again
+    end
+  end
+
+  def destroy
+    @post = Post.where(id: params[:id]).first
+    if @post.destroy
+      redirect_to posts_path
+    else
+      # 
+    end
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :tag_ids => [])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,47 +3,8 @@ class PostsController < ApplicationController
     @posts = Post.order("updated_at DESC")
   end
 
-  def new
-    @post = Post.new
-  end
-
   def show
     @post = Post.where(id: params[:id]).first
   end
 
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path
-    else
-      # render form again
-    end
-  end
-
-  def edit
-    @post = Post.where(id: params[:id]).first
-  end
-
-  def update
-    @post = Post.where(id: params[:id]).first
-    if @post.update_attributes(post_params)
-      redirect_to posts_path
-    else
-      # render edit form again
-    end
-  end
-
-  def destroy
-    @post = Post.where(id: params[:id]).first
-    if @post.destroy
-      redirect_to posts_path
-    else
-      # 
-    end
-  end
-
-  private
-  def post_params
-    params.require(:post).permit(:title, :body, :tag_ids => [])
-  end
 end
